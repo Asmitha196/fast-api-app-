4f624d7e422548a3cea54e8a01918e2e82e754ce
ashmitha <ashmithashetty196@gmail.com>
2026-06-27 14:18:23 +0530

    training

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+from routers import company,job
+from database import Base,engine
+
+app = FastAPI()
+print(engine)
+Base.metadata.create_all(bind=engine)
+
+app.include_router(company.router)
+app.include_router(job.router)
+@app.get("/")
+def read_root():
+    return {"Hello":"World"}
+
+@app.get("/about")
+def read_about():
+    return {"about":"This is sbout page"}
+
+@app.get("/contact")
+def read_root():
+    return {"contact":"This is about contact page "}
+
+
+
+
+
