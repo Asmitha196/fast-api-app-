ea745504b2130c3364bcf5f844a97db22ee5af71
ashmitha <ashmithashetty196@gmail.com>
2026-06-29 16:47:57 +0530

    frontend part 2

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,11 +1,16 @@
 import NavBar from "./components/NavBar";
 import Welcome from "./components/Welcome";
-
+import Footer from "./components/Footer";
+import CompanyCard from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
 function App() {
   return (
     <>
       <NavBar />
       <Welcome />
+      <CompanyCard />
+      <JobCard />
+      <Footer />
     </>
   );
 }
